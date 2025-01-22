5966edb31d90441311540ac08cb3fccd768a950c
vishal.kumar@reckonsys.com <Reckonsys@123>
2025-01-22 20:00:03 +0530

    create FE, show account and connect to web3 and load contracts

---
+++ src/app.js
@@ -0,0 +1,62 @@
+var Web3 = require('web3');
+App = {
+    load: async () => {
+        console.log("app loading...")
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContracts()
+    },
+    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        await ethereum.enable()
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */})
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider
+      window.web3 = new Web3(web3.currentProvider)
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */})
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  },
+
+  loadAccount: async () => {
+    console.log(" Loading Accounts...")
+    App.account = web3.eth.accounts[0]
+    console.log(App.account)
+  },
+  
+  loadContracts: async () => {
+    console.log("Loading Contracts...")
+    const todoList = await $.getJSON("ToDoList.json")
+    console.log(todoList)
+  }
+
+}
+
+
+
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+})
